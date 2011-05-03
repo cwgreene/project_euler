b82fd55397234bbee07cd09b77f9a2e7ce6e6353
Chris Greene <archgoon@gmail.com>
2011-05-03 11:34:36 -0700

    Added euler114

---
+++ solutions/euler114.py
@@ -0,0 +1,14 @@
+def count_blocks(minsize,maxsize,remaining,mcount={}):
+	if (minsize,maxsize,remaining) in mcount:
+		return mcount[(minsize,maxsize,remaining)]
+	total = 0
+	#pick a size
+	for size in range(minsize,maxsize+1):
+		#figure out where to plop it down
+		for start in range(remaining-size+1):
+			total += 1 + count_blocks(minsize,maxsize,
+						remaining-(size+start+1))
+	mcount[(minsize,maxsize,remaining)] = total
+	return total
+print 1+count_blocks(3,30,30)
+
